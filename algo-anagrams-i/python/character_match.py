2d0f0ad032c14aa5f364de60ef94ecc59a9b7be5
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-07 08:43:59 -0400

    Added exercises to git repo

---
+++ algo-anagrams-i/python/character_match.py
@@ -0,0 +1,12 @@
+# Don't forget to run the tests (and create some of your own)
+
+# Part 1
+def is_character_match(string1, string2):
+	# Your code here
+	pass
+
+
+# Part 2
+def anagrams_for(word, list_of_words):
+	# your code here
+	pass
