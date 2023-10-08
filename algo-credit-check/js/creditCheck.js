c891e212047fb9f3074a3d8829c11fefc38247ff
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-08 15:23:41 -0400

    Added solution for js

--- algo-credit-check/js/creditCheck.js
+++ algo-credit-check/js/creditCheck.js
@@ -1,3 +1,29 @@
-exports.creditCheck = function (str) {
-  return true;
+const creditCheck = function (str) {
+  const digits = str
+    .split("")
+    .reverse()
+    .map(Number)
+    .map((elem, ind) => {
+      if (ind % 2 != 0) {
+        return elem * 2;
+      } else {
+        return elem;
+      }
+    })
+    .map((elem) => {
+      if (elem >= 10) {
+        return elem
+          .toString()
+          .split("")
+          .map(Number)
+          .reduce((a, b) => a + b);
+      } else {
+        return elem;
+      }
+    })
+    .reduce((a, b) => a + b);
+  return digits % 2 == 0;
 };
+
+console.log(creditCheck("5541801923795240"));
+module.exports = { creditCheck };
