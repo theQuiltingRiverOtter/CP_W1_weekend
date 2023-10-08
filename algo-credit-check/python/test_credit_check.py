16b9e0b8ab09cf6d88fb4e63c7d624359843c6c2
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-08 14:41:31 -0400

    Fixed pytest parametrize

--- algo-credit-check/python/test_credit_check.py
+++ algo-credit-check/python/test_credit_check.py
@@ -1,15 +1,13 @@
 from credit_check import credit_check
 import pytest
 
-pytest.mark.parametrize(
+
+@pytest.mark.parametrize(
     "valid_number", ["5541808923795240", "4024007136512380", "6011797668867828"]
 )
-
-pytest.mark.parametrize(
+@pytest.mark.parametrize(
     "invalid_number", ["5541801923795240", "4024007106512380", "6011797668868728"]
 )
-
-
 def test_credit_check(valid_number):
     assert credit_check(valid_number)
 
