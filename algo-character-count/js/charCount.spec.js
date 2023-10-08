390ff0e40bf17be5fc1d296b3153dcdcdd118334
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-08 14:24:06 -0400

    Added soluiton and fixed tests to test nested array

--- algo-character-count/js/charCount.spec.js
+++ algo-character-count/js/charCount.spec.js
@@ -2,10 +2,32 @@ const { charCount } = require("./charCount");
 
 describe("Tests charCount for single words", () => {
   test("charCount('aaabbc') == [['a', 3], ['b',2], ['c',1]]", () => {
-    expect(charCount("aaabbc")).toBe([
+    expect(charCount("aaabbc")).toMatchArray([
       ["a", 3],
       ["b", 2],
       ["c", 1],
+    ]);
+  });
+});
+
+describe("Tests charCount for sentence", () => {
+  test('charCount("an apple a day will keep the doctor away") == [["a", 6],["e", 4],["l", 3],["p", 3],["d", 2],["o", 2],["t", 2],["w", 2],["y", 2],["c", 1],["h", 1],["i", 1],["k", 1],["n", 1],["r", 1]]', () => {
+    expect(charCount("an apple a day will keep the doctor away")).toMatchArray([
+      ["a", 6],
+      ["e", 4],
+      ["l", 3],
+      ["p", 3],
+      ["d", 2],
+      ["o", 2],
+      ["t", 2],
+      ["w", 2],
+      ["y", 2],
+      ["c", 1],
+      ["h", 1],
+      ["i", 1],
+      ["k", 1],
+      ["n", 1],
+      ["r", 1],
     ]);
   });
 });
