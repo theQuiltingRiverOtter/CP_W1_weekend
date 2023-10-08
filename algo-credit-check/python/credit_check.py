09d04a4abb544b6bdb056ffd9112e3897158bd89
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-08 14:59:23 -0400

    Added solution, added capsys checking to tests

--- algo-credit-check/python/credit_check.py
+++ algo-credit-check/python/credit_check.py
@@ -1,8 +1,28 @@
+def add_digits(num):
+    digits = str(num)
+    total = 0
+    for digit in digits:
+        total += int(digit)
+    return total
+
+
 def credit_check(str):
-    pass
+    digits_reversed = [int(x) for x in str[::-1]]
+    digits_after_doubling = [
+        x * 2 if i % 2 != 0 else x for i, x in enumerate(digits_reversed)
+    ]
+    digits_after_adding = [
+        add_digits(x) if x >= 10 else x for x in digits_after_doubling
+    ]
+    if sum(digits_after_adding) % 10 == 0:
+        print("The number is valid!")
+        return True
+    else:
+        print("The number is invalid!")
+        return False
+
 
 # Your Luhn Algorithm Here
 # Expected Output:
 # If it is valid, print "The number is valid!"
 # If it is invalid, print "The number is invalid!"
-
--- algo-credit-check/python/test_credit_check.py
+++ algo-credit-check/python/test_credit_check.py
@@ -5,12 +5,16 @@ import pytest
 @pytest.mark.parametrize(
     "valid_number", ["5541808923795240", "4024007136512380", "6011797668867828"]
 )
+def test_credit_check(valid_number, capsys):
+    assert credit_check(valid_number)
+    captured = capsys.readouterr()
+    assert captured.out == "The number is valid!\n"
+
+
 @pytest.mark.parametrize(
     "invalid_number", ["5541801923795240", "4024007106512380", "6011797668868728"]
 )
-def test_credit_check(valid_number):
-    assert credit_check(valid_number)
-
-
-def test_not_credit_check(invalid_number):
+def test_not_credit_check(invalid_number, capsys):
     assert not credit_check(invalid_number)
+    captured = capsys.readouterr()
+    assert captured.out == "The number is invalid!\n"
