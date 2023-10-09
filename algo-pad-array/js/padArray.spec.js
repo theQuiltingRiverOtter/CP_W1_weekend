fd6fb8e0fae8863c34ac4b0a8266e91ebaa7130d
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-09 00:22:03 -0400

    Added test for size smaller than array passed in

--- algo-pad-array/js/padArray.spec.js
+++ algo-pad-array/js/padArray.spec.js
@@ -5,3 +5,21 @@ describe("Tests pad() without a default pad value", () => {
     expect(pad([1, 2, 3], 5)).toMatchArray([1, 2, 3, null, null]);
   });
 });
+
+describe("Tests pad() without a default pad value and a min size smaller than the array passed in", () => {
+  test("Test pad([1,2,3,4,5],3) to return [1,2,3,4,5]", () => {
+    expect(pad([1, 2, 3, 4, 5], 3)).toMatchArray([1, 2, 3, 4, 5]);
+  });
+  test("Test pad([32,5,16,'hello',null],0) to return [32,5,16,'hello',null]", () => {
+    expect(pad([32, 5, 16, "hello", null], 0)).toMatchArray([
+      32,
+      5,
+      16,
+      "hello",
+      null,
+    ]);
+  });
+  test("Test pad([15,6,7],-10) to return [15,6,7]", () => {
+    expect(pad([15, 6, 7], -10)).toMatchArray([15, 6, 7]);
+  });
+});
