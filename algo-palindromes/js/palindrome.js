848643ee9eca13a6bf74801b18e9fc8cedbf1ef3
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-09 02:26:44 -0400

    Added solutions and tests for js

--- algo-palindromes/js/palindrome.js
+++ algo-palindromes/js/palindrome.js
@@ -1,3 +1,27 @@
-exports.palindrome = function(word) {
-
+const palindrome = function (word) {
+  if (typeof word == "number" && word !== null) {
+    const num = word.toString();
+    let num_reverse = "";
+    for (let i = num.length - 1; i >= 0; i--) {
+      num_reverse += num[i];
+    }
+    return num == num_reverse;
+  }
+  if (word.length == 0) {
+    return true;
+  }
+  let new_string = "";
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+  for (let letter of word.toLowerCase()) {
+    if (alphanumeric.indexOf(letter) !== -1) {
+      new_string += letter;
+    }
+  }
+  let reversed_string = "";
+  for (let j = new_string.length - 1; j >= 0; j--) {
+    reversed_string += new_string[j];
+  }
+  return reversed_string == new_string;
 };
+
+module.exports = { palindrome };
--- algo-palindromes/js/palindrome.spec.js
+++ algo-palindromes/js/palindrome.spec.js
@@ -1,7 +1,37 @@
-const { palindrome } = require("./palindrome");
+const { palindrome } = require("./palindrome.js");
 
 describe("Tests palindrome() for numbers", () => {
   test("Test palindrome(434) to be true", () => {
     expect(palindrome(434)).toBeTruthy();
+  });
+  test("Test palindrome(136) to be fasle", () => {
+    expect(palindrome(136)).toBeFalsy();
+  });
+});
+
+describe("Tests palindrome for words", () => {
+  test("Test palindrome('racecar') to be true", () => {
+    expect(palindrome("racecar")).toBeTruthy();
+  });
+  test("Test palindrome('ciVic') to be true", () => {
+    expect(palindrome("ciVic")).toBeTruthy();
+  });
+  test("Test palindrome('Noon') to be true", () => {
+    expect(palindrome("Noon")).toBeTruthy();
+  });
+  test("Test palindrome('nice') to be fasle", () => {
+    expect(palindrome("nice")).toBeFalsy();
+  });
+  test("Test palindrome('bomb') to be fasle", () => {
+    expect(palindrome("bomb")).toBeFalsy();
+  });
+});
+
+describe("Tests palindrome for sentences", () => {
+  test("Test palindrome('Sore was I ere I saw Eros') to be true", () => {
+    expect(palindrome("Sore was I ere I saw Eros")).toBeTruthy();
+  });
+  test("Test palindrome('A man, a plan, a canal -- Panama') to be true", () => {
+    expect(palindrome("A man, a plan, a canal -- Panama")).toBeTruthy();
   });
 });
