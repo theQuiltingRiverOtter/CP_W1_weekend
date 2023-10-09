86fecbd68821333656dbb7713bac70db75329a0f
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-09 02:43:01 -0400

    Added solution for python

--- algo-sum-pairs/python/sum_pairs.py
+++ algo-sum-pairs/python/sum_pairs.py
@@ -1,5 +1,9 @@
 def sum_pairs(input_array: list, target_value: int) -> list:
     sum_pairs_list = []
+    for i in range(len(input_array)):
+        for j in range(i, len(input_array)):
+            if input_array[i] + input_array[j] == target_value:
+                sum_pairs_list.append([input_array[i], input_array[j]])
 
     if len(sum_pairs_list) == 0:
         return "unable to find pairs"
