d1f8fca81644528a208fc63885314455d16c5f47
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-08 23:20:32 -0400

    Added basic test

--- algo-pad-array/python/pad_array.py
+++ algo-pad-array/python/pad_array.py
@@ -1,3 +1,7 @@
-#REMEMBER TO PSEUDOCODE
-def pad(array, min_size, value = None):
-    pass 
+# REMEMBER TO PSEUDOCODE
+def pad(array, min_size, value=None):
+    new_list = array[:]
+    size = min_size - len(array)
+    for i in range(size):
+        new_list.append(value)
+    return new_list
