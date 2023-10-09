c14ee6059dca11e9ac508e8f23b25739444b2fd7
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-09 03:36:03 -0400

    Added solution for js

--- algo-sum-pairs/js/sumPairs.js
+++ algo-sum-pairs/js/sumPairs.js
@@ -1,3 +1,22 @@
-const sumPairs = function (input_array, target_value) {};
+const sumPairs = function (inputArray, targetValue) {
+  const sumPairDict = {};
+  for (let value of inputArray) {
+    if (targetValue - value in sumPairDict) {
+      sumPairDict[targetValue - value] = value;
+    } else {
+      sumPairDict[value] = undefined;
+    }
+  }
+  const sumPairList = [];
+  for (let value in sumPairDict) {
+    if (sumPairDict[value] != undefined) {
+      sumPairList.push([Number(value), sumPairDict[value]]);
+    }
+  }
+  if (sumPairList.length !== 0) {
+    return sumPairList;
+  }
+  return "unable to find pairs";
+};
 
 module.exports = { sumPairs };
